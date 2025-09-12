8b4daea4ce70a98e98caed09a3cae7043661e46a
Rémi Passmoilesel <r.passmoilesel@protonmail.com>
2025-09-12 18:37:25 +0200

    Wip

--- scripts/publish-docker.sh
+++ scripts/publish-docker.sh
@@ -4,7 +4,7 @@ set -x
 set -e
 
 export CI=true
-export DOCKER_TAG="registry.gitlab.com/remipassmoilesel/personal-website/personal-website:0.18"
+export DOCKER_TAG="registry.gitlab.com/remipassmoilesel/personal-website/personal-website:0.19"
 
 pnpm install
 pnpm run build:cv:en
